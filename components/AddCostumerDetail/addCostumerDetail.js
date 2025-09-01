7c49aecf75f7c44346d22a8b4a2e7cbd3eef01ef
Abhishek <abhigoud198484@gmail.com>
2025-09-02 02:24:29 +0530

    Add react useFrom hook

--- components/AddCostumerDetail/addCostumerDetail.js
+++ components/AddCostumerDetail/addCostumerDetail.js
@@ -1,32 +1,56 @@
+"use client";
+
 import styles from "./addCostumerDetail.module.css";
+import { useForm } from "react-hook-form";
 
 export default function AddCostumerDetail() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.background}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <input
           placeholder="Enter costumer name"
           className={styles.input}
           type="text"
-          name="name"
+          {...register("name")}
         />
         <input
           placeholder="Amount"
           className={styles.input}
           type="number"
-          name="number"
+          {...register("number")}
         />
         <div className={styles.status}>
-          <input type="radio" id="tbd" name="status" value="tbd" />
+          <input
+            type="radio"
+            id="tbd"
+            value="To be Delivered"
+            {...register("status")}
+          />
           <label htmlFor="tbd"> To be Delivered</label>
           <br />
-          <input type="radio" id="tbd" name="status" value="tbd" />
-          <label htmlFor="tbd"> Delivery in Progress</label>
+          <input
+            type="radio"
+            id="progress"
+            value="progress"
+            {...register("status")}
+          />
+          <label htmlFor="progress"> Delivery in Progress</label>
           <br />
-          <input type="radio" id="tbd" name="status" value="tbd" />
-          <label htmlFor="tbd"> Delivered</label>
+          <input
+            type="radio"
+            id="delivered"
+            value="delivered"
+            {...register("status")}
+          />
+          <label htmlFor="delivered"> Delivered</label>
         </div>
-        <button className={styles.button}>Submit</button>
+        <button type="submit" className={styles.button}>Submit</button>
       </form>
     </div>
   );
